091284e088070e5d6b520c2b9847ffcf809edd42
Suseela <suseela230304@gmail.com>
2026-08-05 09:43:17 +0530

    jewelsandstones.java

---
+++ jewelsandstones.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        Set<Character> jewels = new HashSet();
+        for (int i = 0; i < J.length(); i++)
+            jewels.add(J.charAt(i));
+        int ans = 0;
+        for (int i = 0; i < S.length(); i++) {
+            if (jewels.contains(S.charAt(i))) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
