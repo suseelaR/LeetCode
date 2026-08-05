5a5b2c519445989b3c67ae5f3c3524903e783f44
Suseela <suseela230304@gmail.com>
2026-08-05 09:36:14 +0530

    bsttogreatertree.java

---
+++ bsttogreatertree.java
@@ -0,0 +1,13 @@
+class Solution {
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root != null) {
+            convertBST(root.right);
+            root.val += sum;
+            sum = root.val;
+            convertBST(root.left);
+        }
+        return root;
+    }
+}
